c27f913b87c966ebbc4d9db637e5124d3af78c22
moisesmartincy <moisesmartincy@gmail.com>
2024-03-23 21:39:32 -0400

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,59 @@
+// En estas primeras 6 preguntas, reemplaza `null` por la respuesta
+
+// Crea una variable "string", puede contener lo que quieras:
+const nuevaString = "hola cracks";
+
+// Crea una variable numérica, puede ser cualquier número:
+const nuevoNum = 7;
+
+// Crea una variable booleana:
+const nuevoBool = false;
+
+// Resuelve el siguiente problema matemático:
+const nuevaResta = 9-6;
+
+// Resuelve el siguiente problema matemático://
+const nuevaMultiplicacion = 9*9;
+
+// Resuelve el siguiente problema matemático:
+const nuevoModulo = 21 % 4 === 1;
+console.log("el modulo es: ",21%5)
+
+/* ----------------------------------------------------------------------------------- */
+
+// Ejercicio 1: Crear variables de diferentes tipos de datos y luego consologuear su valor.
+// a. Crear una variable llamada "nombre" y asignarle tu nombre como cadena.
+let nombre = "moises martin condori yujra";
+console.log(nombre);
+// b. Crear una variable llamada "edad" y asignarle tu edad como número.
+let edad = 23;
+console.log(edad);
+// c. Crear una variable llamada "esEstudiante" y asignarle un valor booleano que indique si eres estudiante o no.
+let esEstudiante = true;
+console.log(esEstudiante);
+// d. Crear una variable llamada "nulo" y asignarle el valor nulo.
+let nulo = null;
+console.log(nulo);
+
+
+
+
+
+/* ----------------------------------------------------------------------------------- */
+// Ejercicio 3: Operaciones matemáticas
+// a. Crear dos variables, "numero1" y "numero2", y asignarles valores numéricos.
+let numero1= 1;
+let numero2= 3;
+
+// b. Calcular la suma, resta, multiplicación y división de los dos números (en nuevas variables) y mostrar los resultados en la consola.
+let suma= numero1+numero2;
+let resta= numero1-numero2;
+let multiplicacion=numero1*numero2;
+let division=numero1/numero2;
+console.log("la suma es: ", suma);
+console.log("la resta es: ", resta);
+console.log("la multiplicacion es: ", multiplicacion);
+console.log("la division es: ", division);
+
+//dia 2
+
